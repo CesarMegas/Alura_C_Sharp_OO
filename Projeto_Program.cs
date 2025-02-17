cfa47cf680876dfaf42361e12e0b50cb1bf5a61f
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-02-17 16:26:19 -0300

    Projeto_Program.cs
    Programa Principal do Projeto.

---
+++ Projeto_Program.cs
@@ -0,0 +1,14 @@
+Musica musica1 = new Musica();
+musica1.nome = "Getsêmani";
+musica1.artista = "Leonardo Gonçalves";
+musica1.duracao = 269;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "SPOT!";
+musica2.artista = "ZICO, JENNIE";
+musica2.duracao = 167;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
