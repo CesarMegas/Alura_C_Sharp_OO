bb23aca780e0202f6a94f8b3953b9c12817985d3
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-02-27 17:45:47 -0300

    Project_Album.cs
    Programa utilizado para criar os Álbuns do Programa Principal.

--- Project_Album.cs
+++ Project_Album.cs
@@ -1,15 +1,25 @@
 class Album 
 {
     private List<Song> songs = new List<Song>();
-    public string Name { get; set; }
 
+    //Constructor of the album
+    public Album(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    //Property that calculates the total duration of an album
     public int TotalDuration => songs.Sum(s => s.Duration);
 
+    //Method that adds songs to an album
     public void AddSong(Song song) 
     {
         songs.Add(song);
     }
 
+    //Method that displays all of the album's songs
     public void DisplayAlbumSongs() 
     {
         Console.WriteLine($"{Name} Song List:\n");
